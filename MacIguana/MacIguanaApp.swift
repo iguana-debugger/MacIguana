5977e7aeaf10dc4f12f621c7ff1084c6fe028c6f
James Harvey <44349936+jmshrv@users.noreply.github.com>
2023-10-24 16:26:15 +0100

    Initial Commit

---
+++ MacIguana/MacIguanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacIguanaApp.swift
+//  MacIguana
+//
+//  Created by James on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MacIguanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
