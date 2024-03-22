4c96d31d63ad1193d6b50c1d115bac9d7c94d1cf
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-03-22 15:20:30 +0000

    Fix duplicate IDs in DisassemblyView

--- MacIguana/DisassemblyView.swift
+++ MacIguana/DisassemblyView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 import Libiguana
 
+/// `KmdParseLine`, with the associated offset. We do this to get `Identifiable` conformance. I did try using
+/// `KmdparseLine`'s `hashValue`, but of course identical lines caused issues.
+///
+/// This struct could probably be made generic :)
+struct IndexedKmdparseLine: Identifiable {
+    var id: Int {
+        offset.hashValue ^ element.hashValue
+    }
+    
+    let offset: Int
+    let element: KmdparseLine
+}
+
 struct DisassemblyView: View {
     public let lines: [KmdparseLine]
     
+    private var lineIndexes: [IndexedKmdparseLine] {
+        lines.enumerated().map { .init(offset: $0.offset, element: $0.element) }
+    }
+    
     var body: some View {
-        Table(lines) {
+        Table(lineIndexes) {
             TableColumn("Address") { line in
-                if let memoryAddress = line.memoryAddress {
+                if let memoryAddress = line.element.memoryAddress {
                     let hex = String(format: "%08X", memoryAddress)
                     Text(hex)
                         .monospaced()
@@ -22,21 +39,21 @@ struct DisassemblyView: View {
             }
             .width(70)
             TableColumn("Hex") { line in
-                if let word = line.word {
+                if let word = line.element.word {
                     Text(word.hex)
                         .monospaced()
                 }
             }
             .width(80)
             TableColumn("String") {line in
-                if let string = line.word?.string {
+                if let string = line.element.word?.string {
                     Text(string)
                         .monospaced()
                 }
             }
             .width(40)
             TableColumn("Disassembly") { line in
-                Text(line.comment)
+                Text(line.element.comment)
                     .monospaced()
             }
         }
